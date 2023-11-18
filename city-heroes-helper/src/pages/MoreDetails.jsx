1f337ba8ab1da94845a5dc9bdb8970ebd2807f7c
Samuel <3.10@web.de>
2023-11-18 16:20:12 +0100

    created buttons

--- city-heroes-helper/src/pages/MoreDetails.jsx
+++ city-heroes-helper/src/pages/MoreDetails.jsx
@@ -4,8 +4,15 @@ import {Link} from "react-router-dom";
 const MoreDetails = () => {
     //const { title, location, timeSlot, additionalInfo } = eventDetails;
 
+
+
     return (
         <div>
+            <div>
+            <button>
+                <Link to='/open-events'>Return Main Page</Link>
+            </button>
+            </div>
             <div>
                 {/* Placeholder for the map - You can integrate a map library here */}
                 <p>Map Placeholder</p>
@@ -14,7 +21,6 @@ const MoreDetails = () => {
             <p>Location: placeholder</p>
             <p>Time Slot: placeholder</p>
             <p>Add. Info</p>
-            <Link to="/accepted-events">Return</Link>
             <button onClick={() => console.log('Accept clicked')}>Accept</button>
             <button onClick={() => console.log('Decline clicked')}>Decline</button>
             <button onClick={() => console.log('Share clicked')}>Share</button>
--- city-heroes-helper/src/pages/OpenEvents.jsx
+++ city-heroes-helper/src/pages/OpenEvents.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import MoreDetails from './MoreDetails';
 import {Link, Route} from "react-router-dom";
-
 const OpenEvents = () => {
 
     const events = [
@@ -10,6 +9,8 @@ const OpenEvents = () => {
         { title: 'Event 3', distance: '8 km', location: 'Location 3', timeSlot: '5:00 PM', additionalInfo: 'Sample info for Event 3' },
     ];
 
+    const pageName = '/open-events';
+
     return (
         <div>
             <h2>Open Events</h2>
@@ -17,7 +18,9 @@ const OpenEvents = () => {
                 <div key={index} className="event-tile">
                     <h3>{event.title}</h3>
                     <p>Distance: {event.distance}</p>
-                    <Link to="/more-details">More details</Link>
+                    <button>
+                        <Link to='/more-details'>More details</Link>
+                    </button>
                     <button onClick={() => console.log('Accept clicked')}>Accept</button>
                     <button onClick={() => console.log('Decline clicked')}>Decline</button>
                 </div>
